5ff4d38eebe5a19ddebb03f1ac3f74c99fb29567
Luan Barbosa <luan.barbosa.gomes@nordea.com>
2020-03-10 15:51:43 +0200

    List expressions saved on remote db.

--- app/src/main/java/com/luanbarbosagomes/hmr/data/Expression.kt
+++ app/src/main/java/com/luanbarbosagomes/hmr/data/Expression.kt
@@ -2,6 +2,7 @@ package com.luanbarbosagomes.hmr.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 enum class Level(val uid: Int) {
@@ -12,13 +13,22 @@ enum class Level(val uid: Int) {
     KNOWN(4)
 }
 
+open class ExpressionLean {
+    @Ignore open var uid: Long? = null
+    @Ignore open lateinit var value: String
+    @Ignore open lateinit var translation: String
+    @Ignore open lateinit var level: Level
+
+    fun toExpression() = Expression(uid, value, translation, level)
+}
+
 @Entity(tableName = "expression")
 data class Expression(
-    @PrimaryKey(autoGenerate = true) val uid: Long? = null,
-    @ColumnInfo val value: String,
-    @ColumnInfo val translation: String,
-    @ColumnInfo val level: Level
-) {
+    @PrimaryKey(autoGenerate = true) override var uid: Long? = null,
+    @ColumnInfo override var value: String,
+    @ColumnInfo override var translation: String,
+    @ColumnInfo override var level: Level
+): ExpressionLean() {
 
     companion object {
         val empty = Expression(value = "", translation =  "", level = Level.NEW)
--- app/src/main/java/com/luanbarbosagomes/hmr/data/repository/RemoteExpressionRepository.kt
+++ app/src/main/java/com/luanbarbosagomes/hmr/data/repository/RemoteExpressionRepository.kt
@@ -1,11 +1,20 @@
 package com.luanbarbosagomes.hmr.data.repository
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.luanbarbosagomes.hmr.App.Companion.currentFirebaseUser
 import com.luanbarbosagomes.hmr.App.Companion.firebaseDb
 import com.luanbarbosagomes.hmr.UserNotFoundException
 import com.luanbarbosagomes.hmr.data.Expression
+import com.luanbarbosagomes.hmr.data.ExpressionLean
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Expression repository responsible for CRUD expressions remotely using Firebase Cloud Firestore.
@@ -25,9 +34,17 @@ class RemoteExpressionRepository @Inject constructor() : IExpressionRepository {
         }
     }
 
-    override suspend fun getAll(): List<Expression> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAll(): List<Expression> =
+        withContext(Dispatchers.IO) {
+            suspendCoroutine<DataSnapshot> { continuation ->
+                dbReference.addListenerForSingleValueEvent(
+                    FirebaseValueEventListener(
+                        onDataChange = { continuation.resume(it) },
+                        onError = { continuation.resumeWithException(it.toException()) }
+                    )
+                )
+            }
+        }.toExpressionList()
 
     override suspend fun deleteAll() {
         TODO("Not yet implemented")
@@ -41,6 +58,23 @@ class RemoteExpressionRepository @Inject constructor() : IExpressionRepository {
         TODO("Not yet implemented")
     }
 
+}
+
+private fun DataSnapshot.toExpressionList(): List<Expression> =
+    this.children
+        .map { it.getValue(ExpressionLean::class.java) }
+        .mapNotNull { it?.toExpression() }
+
+/**
+ * Used to "convert" Firebase Database's fetch listener for a coroutine type handling.
+ * Not my code! https://gist.github.com/Sloy/f68921a2ead6466e530ff4b18c180c4d
+ */
+class FirebaseValueEventListener(
+    val onDataChange: (DataSnapshot) -> Unit,
+    val onError: (DatabaseError) -> Unit
+) : ValueEventListener {
+    override fun onDataChange(data: DataSnapshot) = onDataChange.invoke(data)
+    override fun onCancelled(error: DatabaseError) = onError.invoke(error)
 }
 
 class UnableToSaveError(message: String) : Throwable(message)
